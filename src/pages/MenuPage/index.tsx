96079f71cfab1d0315cb2275b3893a458db76841
Raiany Stein <102999947+raianystein@users.noreply.github.com>
2024-11-06 15:51:22 -0300

    Update index.tsx

--- src/pages/MenuPage/index.tsx
+++ src/pages/MenuPage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import "../../styles/menu.css";
+import { Link } from "react-router-dom";
 
 export default function MenuPage() {
   return (
